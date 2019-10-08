8a0e0c29bdea374a5e83e2692fdedfd4699444f4
esim7 <makishev@yahoo.com>
2019-10-08 15:58:29 +0600

    Загрузка задания

---
+++ ButtonDelegate/Button.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonDelegate
+{
+    public class Button
+    {
+        public event Action PressedAKey;
+
+        public string Text { get; set; }
+        public char Symbol { get; set; }
+        public int Height { get; set; } //высота
+        public int Width { get; set; }
+        ConsoleColor MainColor { get; set; }
+        ConsoleColor BgColor { get; set; }
+
+        public Button(string text, char symbol, int height, int width, ConsoleColor mainColor, ConsoleColor bgColor)
+        {
+            Text = text;
+            Symbol = symbol;
+            Height = height;
+            Width = width;
+            MainColor = mainColor;
+            BgColor = bgColor;
+        }
+
+        public void isPressedButton()
+        {
+            PrintMe();
+            PressedAKey?.Invoke();
+        }
+
+        public void PrintMe()
+        {
+            for(int i = 0; i < this.Height; i++)
+            {
+                for(int j = 0; j < this.Width; j++)
+                {
+                    if ((i == 0 || i == Height - 1) || (j == 0 || j == Width - 1))
+                    {
+                        Console.ForegroundColor = MainColor;
+                        Console.BackgroundColor = BgColor;
+                        Console.Write(Symbol);
+                    }
+                    else if(j == Width /2 - Text.Length / 2 && i == Height / 2)
+                    {
+                        j += Text.Length - 1;
+                        Console.Write(Text);
+                    }
+                    else
+                        Console.Write(' ');                    
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }        
+    }
+}
---
+++ ButtonDelegate/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ButtonDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void GetMyName()
+            {
+                Console.WriteLine("Yessimzhan");
+            }
+            void GetARiddle()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Он программы пишет-пишет,\nЗа компьютером сидит,\nВсе эмоции здесь лишни – \nЛишь исходный код твердит.");
+                Console.ResetColor();
+            }
+
+            void GetAJoke()
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("- Вчера долго пытался объяснить бабуле, что работаю программистом...\n- ???\n-Короче, сошлись на том, что чиню телевизоры и развожу мышей...");
+                Console.ResetColor();
+            }
+
+            void GetASecondJoke()
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Первые 90% кода занимают первые 90% времени на разработку… \nОставшиеся 10% кода занимают еще 90% времени на разработку.");
+                Console.ResetColor();
+            }
+            List<Button> buttons = new List<Button>
+            {
+                new Button("W", '*', 5, 7, ConsoleColor.DarkBlue, ConsoleColor.DarkGreen),           
+                new Button("A", '+', 5, 7, ConsoleColor.DarkGray, ConsoleColor.Red),
+                new Button("S", '-', 5, 7, ConsoleColor.Magenta, ConsoleColor.Yellow),
+                new Button("D", '=', 5, 7, ConsoleColor.DarkCyan, ConsoleColor.White)
+            };
+            buttons[0].PressedAKey += GetMyName;
+            buttons[1].PressedAKey += GetARiddle;
+            buttons[2].PressedAKey += GetAJoke;
+            buttons[3].PressedAKey += GetASecondJoke;
+            foreach (Button button in buttons)
+            {
+                button.PrintMe();
+            }
+            Console.WriteLine("Нажмите пробел для активации кнопок");
+            if (Console.ReadLine() == " ")
+            {
+                Console.Clear();
+                foreach (Button button in buttons)
+                {
+                    button.isPressedButton();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
